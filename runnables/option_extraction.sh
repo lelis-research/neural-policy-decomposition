43e52ddd908bfe4daf6fed1d484f4ccc054d064a
Reza Abdollahzadeh <rezaabdz@beluga2.int.ets1.calculquebec.ca>
2024-07-14 21:06:43 -0400

    feat: added multi model per task mechanism and reset option for Game class

--- runnables/option_extraction.sh
+++ runnables/option_extraction.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32G        # memory per node
+#SBATCH --mem=6G        # memory per node
 #SBATCH --time=0-3:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --account=def-lelis
-#SBATCH --array=0-0
+#SBATCH --array=0-19
 
-source envs/venv/bin/activate # Assuming we have all our environments in  `../envs/`
+source ../envs/venv/bin/activate # Assuming we have all our environments in  `../envs/`
 
-python agent.py
+python ../masked_sub_policies_levin_loss.py
+
+
