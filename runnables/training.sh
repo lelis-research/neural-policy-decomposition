868cdc47ad39e5176d7315469df73b06b4f6ed93
Reza Abdollahzadeh <rezaabdz@beluga3.int.ets1.calculquebec.ca>
2024-07-16 05:04:03 -0400

    fix: environment and python script directory in the training bash script

--- runnables/training.sh
+++ runnables/training.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=6G        # memory per node
-#SBATCH --time=0-3:00      # time (DD-HH:MM)
+#SBATCH --time=0-8:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --account=def-lelis
-#SBATCH --array=0-19
+#SBATCH --array=0-0
 
-module load python/3.10
+source envs/venv/bin/activate # Assuming we have all our environments in  `../envs/`
 
-# source /home/rezaabdz/personal-projects/neurosymbolic-programming/dec_options_baseline_v1_env/bin/activate
-
-python3.10 /home/rezaabdz/personal-projects/neurosymbolic-programming/Dec-Options/Archive/combo4_training.py --seed $SLURM_ARRAY_TASK_ID --size 3 --num_iterations 170000 --reg_coef 0.0000 --clip_range 0.1 --ent_coef 0.1 --learning_rate 0.001 --network_size 24 --log_path "$dir"
+python agent.py
