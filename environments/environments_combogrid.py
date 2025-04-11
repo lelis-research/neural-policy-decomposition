df98c8f94e879ee5bf6cd7c63806b5443c6aca51
Reza Abdollahzadeh <45513883+re0078@users.noreply.github.com>
2025-04-11 01:30:21 -0600

    Ppo option extraction (#2)
    * feat: added PPO agent training
    
    * fix: environment and python script directory in the training bash script
    
    * fix: wrong parameters for ppo
    
    * fix: ppo hyperparameter mismatch
    
    * Modify reset() and add register()
    
    * changed hyperparameters
    
    * ppo.py added and some refactoring
    
    * feat: ppo option extraction pipeline (WIP)
    
    * PPO option extraction debugging (WIP)
    
    * feat: added per grid cell evaluation
    
    * feat: added options to combo_gym environment
    
    * fix: option evaluation on each cell now uses the masks
    
    * refactor: changed project structure, file names, and file structures project-wide
    
    * Major restructuring and refactoring.
    The pipeline for retraining with options completed.
    
    * updated .gitignore
    
    * feat: added logits loss for actor critic agents and paralellized hill climbing
    
    * updated requirements.pip
    
    * fix: requirements versions, option extraction script, and some refactoring
    
    * feat: changed process pool to thread pool
    
    * removed redundant logging messages
    
    * feat: added all segments hill climbing
    
    * feat: formalized script inputs and outputs for train_ppo.py + general refactoring
    
    * feat: separating different parts of extract_subpolicy_ppo
    
    * feat: added ProcessPool to hillclimbing algorithm
    
    * fix: bug saving options
    
    * fix: CC+PyTorch bug temporary fix: added new backend
    
    * refactored hc_all_segments
    
    * feat: added trajectory visualization script
    
    * refactor: all_segments hill climbing
    
    * fix: .gitignore issue fixed
    
    * fix: debugging test_by_training (WIP)
    
    * fix: logging bug
    
    * differentiable option leanring
    
    * feat: visualize environments (WIP), added seed to extract_subpolicy_ppo, general refactoring
    
    * feat: saving options based on seeds
    
    * minor refactor
    
    * feat: added test_by_training.sh
    
    * feat: added seed to test_by_training
    
    * refactor: train_ppo
    
    * feat: sparse initialization
    
    * refactor: package name typo
    
    * refactoring
    
    * feat: added wandb to extract_subpolicy_ppo and test_by_training
    
    * Merged with origin
    
    * feat: seed in train_ppo and sparse initialization
    
    * added discrete mask and general refactor
    
    * major refactor
    
    * fixed simplecrossing setting
    
    * feat: added Combogrid to the pipeline and multiple goal Combogrid variations
    
    * fix: bug in combogrid string parsing
    
    * fix: option size combogrid
    
    * test primary models added
    
    * bugs fixed and test on every cell modified
    
    * added a todo
    
    * debug deterministic ppo decision
    
    * General refactor
    
    * refactor, added input masks, and softmax mask transformation
    
    * change file name
    
    * fix: minor bugs in reference
    
    * fix: relu use in PPOAgent and option selection
    
    * feat: added new arguments to option_discovery.py
    
    * feat: added local search option selection
    
    * debug: concurrency issue in option local search
    
    ---------
    
    Co-authored-by: Reza Abdollahzadeh <rezaabdz@beluga3.int.ets1.calculquebec.ca>
    Co-authored-by: Reza Abdollahzadeh <rezaabdz@beluga1.int.ets1.calculquebec.ca>
    Co-authored-by: unknown <behdin.p@gmail.com>
    Co-authored-by: Reza Abdollahzadeh <rezaabdz@beluga2.int.ets1.calculquebec.ca>
    Co-authored-by: Reza Abdollahzadeh <rezaabdz@gra-login2.graham.sharcnet>
    Co-authored-by: Reza Abdollahzadeh <rezaabdz@cedar1.int.cedar.computecanada.ca>

--- environments/environments_combogrid.py
+++ environments/environments_combogrid.py
@@ -1,39 +1,93 @@
-import random
 import numpy as np
 import math
 import gc
+
+
+SEEDS = {
+    "TL-BR": 0,
+    "TR-BL": 1, 
+    "BR-TL": 2, 
+    "BL-TR": 3,
+
+    "ML-BR": 4,
+    "ML-TR": 5,
+    "MR-TL": 6,
+    "MR-BL": 7,
+    "TL-MR": 8,
+    "BL-MR": 9,
+    "TR-ML": 10,
+    "BR-ML": 11,
+
+    "BL-MR-ML-BM-TM": 12,
+    "BL-MR-BM": 13
+}
+
+PROBLEM_NAMES = list(SEEDS.keys())
+
+DIRECTIONS = {(0,0,1): "U",
+              (0,1,2): "D",
+              (2,1,0): "L",
+              (1,0,2): "R"}
 
 class Problem:
     def __init__(self, rows, columns, problem_str):
         self.rows = rows
         self.columns = columns
-        self.initial, self.goal = self._parse_problem(problem_str)
+        self.initial, self.goals = self._parse_problem(problem_str)
+        self.reset()
 
     def _parse_problem(self, problem_str):
-        initial = self._parse_position(problem_str[:2])
-        goal = self._parse_position(problem_str[3:])
-        return initial, goal
+        problem_parts = problem_str.split("-")
+        initial = self._parse_position(problem_parts[0])
+        goals = [self._parse_position(goal_str) for goal_str in problem_parts[1:]]
+        return initial, goals
 
     def _parse_position(self, pos_str):
-        if pos_str[0] == 'T':
+        v_loc, h_loc = pos_str[0], pos_str[1]
+        if v_loc == 'T':
             row = 0
-        elif pos_str[0] == 'B':
+        elif v_loc == 'B':
             row = self.rows - 1
-        elif pos_str[0] == 'M':
+        elif v_loc == 'M':
             row = math.floor(self.rows / 2)
         else:
             raise ValueError("Invalid row specifier. Use 'T' for top, 'B' for bottom, or 'M' for middle.")
         
-        if pos_str[1] == 'L':
+        if h_loc == 'L':
             col = 0
-        elif pos_str[1] == 'R':
+        elif h_loc == 'R':
             col = self.columns - 1
-        elif pos_str[0] == 'M':
+        elif h_loc == 'M':
             col = math.floor(self.columns / 2)
         else:
             raise ValueError("Invalid column specifier. Use 'L' for left, 'R' for right, or 'M' for middle.")
         
         return (row, col)
+    
+    def update_goal(self) -> bool:
+        """
+        Updates the goal coordinations.
+
+        Returns whether or not all goals have been reached.
+        
+        Returns:
+            bool: True if all goals have been reached, False otherwise.
+        """
+
+        if self.goal_idx == len(self.goals) - 1:
+            return True
+        
+        self.goal_idx += 1
+        self.goal = self.goals[self.goal_idx]
+        return False
+    
+    def reset(self):
+        self.goal_idx = 0
+        self.goal = self.goals[self.goal_idx]
+
+    def is_goal(self, loc):
+        return any([(loc[0] == goal[0]) and (loc[1]==goal[1]) for goal in self.goals])
+        
 
 
 class Game:
@@ -46,19 +100,12 @@ class Game:
         self._columns = columns
 
         self.problem = Problem(rows, columns, problem_str)
+        self._matrix_structure = np.zeros((rows, columns))
         
         if init_x and init_y:
             self.reset((init_x, init_y))
         else:
             self.reset()
-
-        self._matrix_structure = np.zeros((rows, columns))
-        self._matrix_goal = np.zeros((rows, columns))        
-
-        goal = self.problem.goal
-        
-        
-        self._matrix_goal[goal[0]][goal[1]] = 1
 
         # state of current action sequence
         """
@@ -78,11 +125,18 @@ class Game:
 
     def reset(self, init_loc=None):
         self._matrix_unit = np.zeros((self._rows, self._columns))
+        self.problem.reset()
         initial = self.problem.initial
         self._x, self._y = init_loc if init_loc else initial
         self._matrix_unit[self._x][self._y] = 1
         self._state = []
+        self.set_goal()   
         gc.collect()
+
+    def set_goal(self):
+        self._matrix_goal = np.zeros((self._rows, self._columns))
+        goal = self.problem.goal
+        self._matrix_goal[goal[0]][goal[1]] = 1
 
     def __repr__(self) -> str:
         str_map = ""
@@ -94,6 +148,8 @@ class Game:
                      str_map += " B "
                 elif self._matrix_goal[i][j] == 1:
                      str_map += " G "
+                elif self.problem.is_goal((i,j)):
+                     str_map += " g "
                 else: 
                      str_map += " 0 "
             str_map += "\n"
@@ -108,6 +164,8 @@ class Game:
                      str_map += " B "
                 elif self._matrix_goal[i][j] == 1:
                      str_map += " G "
+                elif self.problem.is_goal((i,j)):
+                     str_map += " g "
                 elif (i,j) in options and tuple(options[(i,j)]) in self._action_pattern:
                     str_map += f" {option_letters[self._action_pattern[tuple(options[(i,j)])]]} "
                 elif self._matrix_unit[i][j] == 1:
@@ -122,9 +180,13 @@ class Game:
         for i, v in enumerate(self._state):
             one_hot_matrix_state[v][i] = 1
         return np.concatenate((self._matrix_unit.ravel(), one_hot_matrix_state.ravel(), self._matrix_goal.ravel()))
-       
+    
     def is_over(self):
-        return self._matrix_goal[self._x][self._y] == 1
+        if self._matrix_goal[self._x][self._y] == 1:
+            done = self.problem.update_goal()
+            self.set_goal()
+            return done
+        return False
     
     def get_actions(self):
         return [0, 1, 2]
@@ -168,3 +230,13 @@ class Game:
                         self._y += 1
                         self._matrix_unit[self._x][self._y] = 1
             self._state = []
+
+class basic_actions:
+    def __init__(self, action):
+        self.action = action
+
+    def predict(self, x):
+        return self.action
+
+    def predict_hierarchical(self, x, epsilon):
+        return self.predict(x)
--- scripts/test_grid_generalization.sh
+++ scripts/test_grid_generalization.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=6G        # memory per node
-#SBATCH --time=0-12:00      # time (DD-HH:MM)
+#SBATCH --time=0-0:15     # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
-#SBATCH --account=def-lelis
-#SBATCH --array=7-7
+#SBATCH --account=rrg-lelis
+#SBATCH --array=0-3,5-9
+
+module load flexiblas
+export FLEXIBLAS=blis2
 
 source envs/venv/bin/activate # Assuming we have all our environments in  `../envs/`
 
-python agent.py
+OMP_NUM_THREADS=1 python -m pipelines.test_grid_generalization --seed=$SLURM_ARRAY_TASK_ID
